b5af8dd3138007adbc7f6009cff7adcc0e0fe433
IeshaMcNair <ieshamcnair@yahoo.com>
2019-12-07 11:42:43 -0500

    added remote mysql connection details

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,15 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    //$host = '127.0.0.1';
+    //$db = 'attendance_db';
+    //$user = 'root';
+    //$pass = '';
+    //$charset = 'utf8mb4';
+
+    //Remote DB Connection
+    $host = 'remotemysql.com';
+    $db = 'NuGGkpGRze';
+    $user = 'NuGGkpGRze';
+    $pass = 'jmD1mb0oKq';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host; dbname=$db;charset=$charset";
